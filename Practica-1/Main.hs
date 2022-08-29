d291db8836710b55ad832ff858aca759bb65b478
David Rivera Morales <driveramorales46@gmail.com>
2022-08-28 23:48:43 -0500

    Finished practica but I want to use another approach to solve the last 2 subsections

--- Practica-1/Main.hs
+++ Practica-1/Main.hs
@@ -1,3 +1,4 @@
+-- DAVID RIVERA MORALES 320176876
 
 -- La función par n, que recibe un entero y contesta si es o no un número par.
 par :: Int ->  IO [Char]
@@ -26,6 +27,20 @@ divE n m = mod n m
 -- Tu propia versión de head y tail que se llamen cabeza y cola respectivamente.
 cabeza :: [Int] -> Int 
 cabeza list = list!!0
+
+cola :: [Int] -> [Int] 
+cola list = drop 1 list
+
+-- La función quita n lst, que recibe un número entero positivo n y una lista lst para devol-
+-- ver lst pero sin sus primeros n elementos.
+quita :: Int -> [Int] -> [Int]
+quita n lst = drop n lst
+
+-- La función enesimo n lst que toma un entero n y una lista lst para regresar el enésimo
+-- elemento de lst. Los computologos siempre empezamos a contar desde el cero.
+enesimo :: Int -> [Int] -> Int
+enesimo n lst = lst!!(n)
+
 
 
 
