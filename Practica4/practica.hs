639afb8914613a7e511328225987d2b1f9997beb
David Rivera Morales <driveramorales46@gmail.com>
2022-11-02 18:57:13 -0600

    finished pset

--- Practica4/practica.hs
+++ Practica4/practica.hs
@@ -25,7 +25,6 @@ vars (Disy x y) = vars x ++ vars y
 vars (Impl x y) = vars x ++ vars y
 vars (Syss x y) = vars x ++ vars y
 
--- Pattern matching not working with all cases
 asocia_der :: LProp -> LProp
 asocia_der (Conj (Conj x y) z) = Conj x (Conj y z)
 asocia_der (Conj x (Conj y z)) = Conj x (Conj y z)
@@ -37,7 +36,6 @@ asocia_der (Syss (Syss x y) z) = Syss x (Syss y z)
 asocia_der (Syss x (Syss y z)) = Syss x (Syss y z)
 asocia_der x = x
 
--- Pattern matching not working with all cases
 asocia_izq :: LProp -> LProp
 asocia_izq (Conj (Conj x y) z) = Conj (Conj x y) z
 asocia_izq (Conj x (Conj y z)) = Conj (Conj x y) z
@@ -49,7 +47,6 @@ asocia_izq (Syss (Syss x y) z) = Syss (Syss x y) z
 asocia_izq (Syss x (Syss y z)) = Syss (Syss x y) z
 asocia_izq x = x
 
--- Pattern matching not working with all cases
 conm :: LProp -> LProp
 conm (Conj x y) = Conj y x
 conm (Disy x y) = Disy y x
@@ -57,12 +54,13 @@ conm (Impl x y) = Impl y x
 conm (Syss x y) = Syss y x
 conm x = x
 
--- Pattern matching not working with all cases
 dist :: LProp -> LProp
 dist (Conj x (Disy y z)) = Disy (Conj x y) (Conj x z)
 dist (Conj (Disy y z) x) = Disy (Conj y x) (Conj z x)
 dist (Disy x (Conj y z)) = Conj (Disy x y) (Disy x z)
-dist (Disy (Conj y z) x) = Conj (Disy y x) (Disy z x)
+dist (Disy (Conj y z) x) = Conj (Disy y x) (Disy z x )
+dist (Impl x y) = Impl x y
+dist (Syss x y) = Syss x y
 dist x = x
 
 
@@ -100,7 +98,19 @@ num_conectivos (Disy x y) = 1 + num_conectivos x + num_conectivos y
 num_conectivos (Impl x y) = 1 + num_conectivos x + num_conectivos y
 num_conectivos (Syss x y) = 1 + num_conectivos x + num_conectivos y
 
+interpretacion:: LProp -> Asignacion -> Int
+interpretacion PTrue asig = 1
+interpretacion PFalse asig = 0
+interpretacion (Var a) asig = asignaValor a asig
+interpretacion (Neg    expr)  vs = (interpretacion expr vs)-1
+interpretacion (Conj   exp1 exp2) vs = if (interpretacion exp1 vs) == 1 && (interpretacion exp2 vs) == 1 then 1 else 0
+interpretacion (Disy   exp1 exp2) vs = if (interpretacion exp1 vs) == 0 || (interpretacion exp2 vs) == 0 then 1 else 0 --check this
+interpretacion (Impl   exp1 exp2) vs = if (interpretacion exp2 vs) == 1 || (interpretacion exp1 vs)-1 == 0 then 0 else 1
+interpretacion (Syss exp1 exp2) vs = if (interpretacion exp1 vs) ==  (interpretacion exp2 vs) then 1 else 0
 
+--Funcion que dada una asignación, coloca a cada letra el valor requerido. Auxiliar de interpretación 
+asignaValor:: Eq a => a -> [(a,b)] -> b
+asignaValor x ((a,b):xs) = if a == x then b else asignaValor x xs
 
 
 
