be6aafff88342c2f10c523182aa36d1e692531f1
David Rivera Morales <driveramorales46@gmail.com>
2022-11-13 14:30:20 -0600

    need to finish todo

--- Practica5/src/practica.hs
+++ Practica5/src/practica.hs
@@ -1,22 +1,30 @@
-
 --Alias para variables
 type Nombre = String
+
 --Tipo para las proposiciones
-data LProp = T | F | VarP Nombre | Conj LProp LProp | Disy LProp LProp |
-                Impl LProp LProp | Syss LProp LProp | Neg LProp deriving (Show, Eq)
+data LProp
+  = T
+  | F
+  | VarP Nombre
+  | Conj LProp LProp
+  | Disy LProp LProp
+  | Impl LProp LProp
+  | Syss LProp LProp
+  | Neg LProp
+  deriving (Show, Eq)
 
 --Tipo para el Tableaux
-data Tableaux = Hoja [LProp] | Alpha [LProp] Tableaux |
-                 Beta [LProp] Tableaux Tableaux deriving (Show, Eq)
-
-
-
+data Tableaux
+  = Hoja [LProp]
+  | Alpha [LProp] Tableaux
+  | Beta [LProp] Tableaux Tableaux
+  deriving (Show, Eq)
 
 {- literales (1 punto) Función que nos dice si en una lista de fórmulas, todas son literales. -}
 
 literales :: [LProp] -> Bool
 literales [] = True
-literales (x:xs) = case x of
+literales (x : xs) = case x of
   VarP _ -> literales xs
   Neg (VarP _) -> literales xs
   _ -> False
@@ -25,7 +33,7 @@ literales (x:xs) = case x of
 
 nextF :: [LProp] -> LProp
 nextF [] = error "No hay fórmulas"
-nextF (x:xs) = case x of
+nextF (x : xs) = case x of
   VarP _ -> nextF xs
   Neg (VarP _) -> nextF xs
   _ -> x
@@ -41,23 +49,48 @@ alpha (Conj x y) = case x of
     Impl _ _ -> False
     _ -> True
 alpha (Neg (Disy x y)) = case x of
+  Conj _ _ -> False
+  Impl _ _ -> False
+  _ -> case y of
     Conj _ _ -> False
     Impl _ _ -> False
-    _ -> case y of
-        Conj _ _ -> False
-        Impl _ _ -> False
-        _ -> True
+    _ -> True
 alpha (Neg (Impl x y)) = case x of
+  Conj _ _ -> False
+  Disy _ _ -> False
+  _ -> case y of
     Conj _ _ -> False
     Disy _ _ -> False
-    _ -> case y of
-        Conj _ _ -> False
-        Disy _ _ -> False
-        _ -> True
-
+    _ -> True
 alpha _ = False
 
-{- beta (1 punto) Nos dice si una fórmula f es una fórmula β -}
+{- beta (1 punto) Nos dice si una fórmula f es una fórmula β  con la forma (β1 ∨ β1)  ¬(β1 ∧ β1) (β1 → β1) -}
+
+beta :: LProp -> Bool
+{- beta (Disy (VarP "p") (Conj (VarP "r") (VarP "s"))) = True -}
+
+beta (Disy x y) = case x of
+  Conj _ _ -> False
+  Impl _ _ -> False
+  _ -> case y of
+    Conj _ _ -> True
+    Impl _ _ -> True
+    _ -> True
+beta (Neg (Conj x y)) = case x of
+  Disy _ _ -> False
+  Impl _ _ -> False
+  _ -> case y of
+    Disy _ _ -> False
+    Impl _ _ -> False
+    _ -> True
+beta (Neg (Impl x y)) = case x of
+  Conj _ _ -> False
+  Disy _ _ -> False
+  _ -> case y of
+    Conj _ _ -> False
+    Disy _ _ -> False
+    _ -> True
+beta _ = False
 
 {- sigma (1 punto) Nos dice si una fórmula f es una fórmula σ , las fórmulas σ ¬¬σ ¬True ¬False -}
 
@@ -72,20 +105,23 @@ sigma _ = False
 
 expSigma :: [LProp] -> LProp -> [LProp]
 expSigma [] _ = []
-expSigma (x:xs) f = case x of
-    Neg (Neg y) -> (y) : expSigma xs f
-    Neg T -> (F) : expSigma xs f
-    Neg F -> (T) : expSigma xs f
-    _ -> x : expSigma xs f
+expSigma (x : xs) f = case x of
+  Neg (Neg y) -> (y) : expSigma xs f
+  Neg T -> (F) : expSigma xs f
+  Neg F -> (T) : expSigma xs f
+  _ -> x : expSigma xs f
 
 {- expAlpha (1 punto) Dada una lista de fórmulas l y una fórmula f, realiza la expansión alpha de f sobre la lista l. -}
 
 expAlpha :: [LProp] -> LProp -> [LProp]
 expAlpha [] _ = []
-expAlpha (x:xs) f = case x of
-    Conj y z -> (y) : (z) : expAlpha xs f
-    Neg (Disy y z) -> (Neg y) : (Neg z) : expAlpha xs f
-    Neg (Impl y z) -> (y) : (Neg z) : expAlpha xs f
-    _ -> x : expAlpha xs f
+expAlpha (x : xs) f = case x of
+  Conj y z -> (y) : (z) : expAlpha xs f
+  Neg (Disy y z) -> (Neg y) : (Neg z) : expAlpha xs f
+  Neg (Impl y z) -> (y) : (Neg z) : expAlpha xs f
+  _ -> x : expAlpha xs f
 
-{- expBeta (1 punto) Dada una lista de fórmulas l y una fórmula f, realiza la expansión beta de f sobre la lista l. -}
+{- expBeta (1 punto) Dada una lista de fórmulas l y una fórmula f, realiza la expansión beta de f sobre la lista l. con las reglas (β1 ∨ β1)=>β1 β2 ¬(β1 ∧ β1)=>¬β1 ¬β2 (β1 → β1)=>¬β1 β2 -}
+
+-- todo 
+-- make expBeta and correct beta and constTableaux
